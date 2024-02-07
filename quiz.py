948f1bd6eba78581f7739d9468bf68ef359aedca
QuitoTactico <teteban0917@gmail.com>
2024-02-07 15:26:21 -0500

    Quiz XD

--- quiz.py
+++ quiz.py
@@ -1,7 +1,11 @@
 x = 7/13
-xg = round(x, 13)
+#xg = round(x, 12)
+xg = 0.538461538461
 
 E = abs(x-xg)
 e = E/x
 
+# hola
+print(x)
+print(xg)
 print(e)
---
+++ quiz3.py
@@ -0,0 +1,4 @@
+er = 5e-5
+x = 0.13635995
+
+#sacar las cifras significativas
